2a60a1a3d68fe85d69b6accae7ab1ace0b522035
Amol Telang <telangamol0909@gmail.com>
2021-06-07 01:05:11 +0530

    added post model

---
+++ server/models/Post.js
@@ -0,0 +1,44 @@
+const mongoose=require('mongoose');
+
+const PostSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users'
+    },
+    name:{
+        type:String,
+    },
+    location:{
+        type:String
+    },
+    quantity:{
+        type:String
+    },
+    address:{
+        type:String
+    },
+    phno:{
+        type:String
+    },
+    type:{
+        type:String
+    },
+    price:{
+        type:String
+    },
+    homedelivery:{
+        type:String
+    },
+    docrequired:{
+        type:String
+    },
+    text:{
+        type:String
+    }
+
+    
+
+
+});
+
+module.exports=User=mongoose.model(`user`,UserSchema); 
