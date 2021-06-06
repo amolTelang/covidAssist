c877d1bc289fc0806586d4b1639eeab543a15901
Amol Telang <telangamol0909@gmail.com>
2021-06-06 20:20:29 +0530

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    phno:{
+        type:String,
+        required:true,
+        unique:true
+    }
+
+});
+
+module.exports=User=mongoose.model(`user`,UserSchema); 
--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const app=express();
 //connect database
 connectDB();
 
+//init Middelware
+app.use(express.json({extended:false}))
+
 app.get('/',(req,res)=>res.send('API Runnign'));
 
 //define routes
