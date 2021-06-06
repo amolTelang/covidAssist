ff460e68f3795c13cbd8866b061ce2a2219d96d2
Amol Telang <telangamol0909@gmail.com>
2021-06-06 20:12:15 +0530

    added api routes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,19 @@ const connectDB=require('./config/db');
 
 const app=express();
 
+//connect database
 connectDB();
+
 app.get('/',(req,res)=>res.send('API Runnign'));
+
+//define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+
+
+
 const PORT= process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`server has started on ${PORT}`));
