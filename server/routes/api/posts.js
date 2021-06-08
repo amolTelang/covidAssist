ccbf76533530d91571a36d015b98e080035fd8a8
Amol Telang <telangamol0909@gmail.com>
2021-06-08 16:27:04 +0530

    added 3 more routes

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -10,8 +10,7 @@ const User=require('../../models/User');
 //@access private
 router.post('/bds',auth,async(req,res)=>{
     try {
-        console.log(req.user);
-        // const usrid=req.phone.user.id;
+       
         const user=await User.findById(req.user.id);
     
         const newPost=new Post({
@@ -36,6 +35,71 @@ router.post('/bds',auth,async(req,res)=>{
 
 
 
-});                                         
+});     
+
+
+//@router POST api/posts/bds
+//@desc post a BedShelter request 
+//@access private
+router.post('/oxygen',auth,async(req,res)=>{
+    try {
+       
+        const user=await User.findById(req.user.id);
+    
+        const newPost=new Post({
+            user:req.user.id,
+            name:user.name,
+            location:req.body.location,
+            quantity:req.body.quantity,
+            phone:req.body.phone,
+            text:req.body.text,
+            lastTimeVerified:req.body.lastTimeVerified
+        });
+
+        //return post object to database
+        const post=await newPost.save();
+        res.json(post);
+
+
+    } catch (error) {
+        console.error(error.message);98
+        res.status(500).send('server error')
+    }
+
+
+
+});    
+
+//@router POST api/posts/bds
+//@desc post a BedShelter request 
+//@access private
+router.post('/medicine',auth,async(req,res)=>{
+    try {
+       
+        const user=await User.findById(req.user.id);
+    
+        const newPost=new Post({
+            user:req.user.id,
+            name:user.name,
+            location:req.body.location,
+            quantity:req.body.quantity,
+            phone:req.body.phone,
+            text:req.body.text,
+            lastTimeVerified:req.body.lastTimeVerified
+        });
+
+        //return post object to database
+        const post=await newPost.save();
+        res.json(post);
+
+
+    } catch (error) {
+        console.error(error.message);98
+        res.status(500).send('server error')
+    }
+
+
+
+});    
 
 module.exports=router;
