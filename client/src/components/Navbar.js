21f9a3a048da2a5941630a343dea66a7783f1d5c
Amol Telang <telangamol0909@gmail.com>
2021-06-15 00:39:15 +0530

    responsive navbar added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import {Link} from 'react-router-dom'
-function Navbar() {
-    return (
-      <header className="text-gray-600 body-font">
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+const  Navbar=({auth:{isAuthenticated}})=> {
+  const authLinks=(<header className="text-gray-600 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+     
+      <span className="ml-3 text-xl">Covid-Assist</span>
+    </Link>
+    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+    <Link className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Oxygen</Link>
+    <Link className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Medicine</Link>
+      <Link className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Shelter</Link>
+      <Link to='/login' className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login</Link>
+    </nav>
+   
+  </div>
+</header>)
+  const guestLinks=(<header className="text-gray-600 body-font">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
      
@@ -14,11 +31,21 @@ function Navbar() {
     </nav>
    
   </div>
-</header>
-         
-    
-    )
+</header>);
+  return (
+    <>
+  {isAuthenticated ? authLinks : guestLinks }
+    </>
+  )
 }
 
-export default Navbar;
+Navbar.propTypes={
+  auth:PropTypes.object.isRequired
+}
+
+const mapStateToProps=state=>({
+auth:state.auth
+})
+
+export default connect(mapStateToProps,{})(Navbar);
 
